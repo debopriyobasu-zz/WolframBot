3da9dae5ca393091270f7ef2da98332f657288f5
Debopriyo Basu <debopriyobasu@gmail.com>
2018-03-02 12:13:38 +0530

    added backend

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,13 +1,75 @@
 from src import app
-from flask import render_template
-# from flask import jsonify
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+import json
+import requests
+from flask import request
+from flask import make_response, jsonify
+
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return "Hello World!"
 
-# Uncomment to add a new URL at /new
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == 'GET':
+        return "webhook page"
+    else:
+        req = request.get_json(silent=True, force=True)
 
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
+        print("Request:")
+        dat = req['result']['parameters'].get('text')
+        print(dat)
+
+        res = process_req(req)
+        resq = json.dumps(res)
+
+        print(resq)
+        return resq
+
+def process_req(req):
+    action = req.get('result').get('action')
+    print(action)
+    if action == "query":
+        qry = req['result']['parameters'].get('text')
+        print(qry)
+        res = wa_search(qry)
+        return res
+    else:
+        return {
+            "speech" : "error",
+            "displayText" : "error",
+            "data": {},
+            "contextOut": [],
+            "source": "wolfram_alpha_bot"
+        }
+
+
+def wa_search(query):
+    url = 'http://api.wolframalpha.com/v1/result?appid=UJKYEW-YKL88PHUER'
+    srch = ''.join(query)
+    srch_str = srch.replace(' ','+')
+    print(srch_str)
+    final_url = url + "&i=" + srch_str + "%3f"
+
+    # obtain response from Wolfram Alpha
+
+    obj = requests.get(final_url)
+    data = obj.text
+
+    print(data)
+
+    # return response in form understandable by bot
+    return {
+        "speech" : data,
+        "displayText" : data,
+        "data" : {},
+        "contextOut" : [],
+        "source" : "wolfram_alpha_bot"
+    }
+
+
+if __name__ == '__main__':
+    app.run()
